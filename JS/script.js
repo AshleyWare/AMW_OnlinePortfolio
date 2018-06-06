ba9bfbd66dacc773e2e4d8b2fee9bfa2925b962b
Ashley Ware <aware2011@gmail.com>
2018-06-05 19:23:19 -0500

    Initial commit

---
+++ JS/script.js
@@ -0,0 +1,14 @@
+//change background of input fields
+  function changeColor() {
+    var inputVal = document.getElementById("FirstName", "LastName", "Email");
+    if (inputVal.value == "") {
+        inputVal.style.backgroundColor = "yellow";
+    }
+    else{
+        inputVal.style.backgroundColor = "";
+    }
+}
+
+//change nav size after certain outputs
+
+  
